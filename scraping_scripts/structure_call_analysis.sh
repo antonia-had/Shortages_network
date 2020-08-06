3b8e4bc8e28c1976348807700e2fc969b152c105
antonia-had <ah986@cornell.edu>
2020-08-06 17:44:07 -0400

    added venv in bash

--- scraping_scripts/structure_call_analysis.sh
+++ scraping_scripts/structure_call_analysis.sh
@@ -7,5 +7,6 @@
 #SBATCH -t 0:10:00            # set max wallclock time
 
 module load python/3.6.9
+source /home/fs02/pmr82_0001/ah986/envs/shortage_network/bin/activate
 mpirun python3 submit_requests_parallel.py
 
