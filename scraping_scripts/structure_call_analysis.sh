3b6c8412b6cbb3f0511fbd64b82605a3c3424b68
antonia-had <ah986@cornell.edu>
2020-08-06 17:16:47 -0400

    set up MPI scheduler

--- scraping_scripts/structure_call_analysis.sh
+++ scraping_scripts/structure_call_analysis.sh
@@ -1,98 +1,11 @@
 #!/bin/bash
-(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required
-# The above line ensures that the script can be run on Cygwin/Linux even with
-# Windows CRNL line endings.
+#SBATCH --job-name="CDSS_requests"
+#SBATCH --output="requests.out"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --export=ALL
+#SBATCH -t 0:10:00            # set max wallclock time
 
-# structure_call_analysis.sh
-# This script is adapted from @Nightsphere's telemetrystation-15min.sh
-# from https://github.com/OpenCDSS/cdss-rest-services-examples
+module load python/3.6.9
+mpirun python3 submit_requests_parallel.py
 
-# Simple script to run Python program to query the HydroBase web services for
-# historical structure call analysis data for each right in the UCRB
-#
-# Run with --help to see the Python program's arguments.
-
-# Supporting functions, alphabetized...
-
-checkOperatingSystem() {
-	# Check the operating system so script is portable
-	if [[ -n "${operatingSystem}" ]]; then
-		# Have already checked operating system so return
-		return
-	fi
-	operatingSystem="unknown"
-	os=$(uname | tr '[:lower:]' '[:upper:]')
-	case "${os}" in
-		CYGWIN*)
-			operatingSystem="cygwin"
-			;;
-		LINUX*)
-			operatingSystem="linux"
-			;;
-		MINGW*)
-			operatingSystem="mingw"
-			;;
-		DARWIN*)
-			operatingSystem="macos"
-			;;
-	esac
-}
-
-# Determine the Python version to use
-determinePythonVersion()
-{
-	# Version is printed to stderr or stdout so a bit trickier to redirect
-	# https://stackoverflow.com/questions/2342826/how-to-pipe-stderr-and-not-stdout
-	# First try the general Python launcher
-	pythonVersion=$(py --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1)
-	usePy="false"
-
-	if [[ "${pythonVersion}" != "3" ]]; then
-		# Did not find the correct py version so try whether python3 is found
-		pythonVersion=$(python3 --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1)
-
-		if [[ ${pythonVersion} != "3" ]]; then
-		echo Error: structure_call_analysis.py can only be run using Python Version 3
-		echo Please update to the most up-to-date version try again
-		exit 1
-		fi
-	else
-		# py was found
-		usePy="true"
-	fi
-}
-
-######## ENTRY POINT FOR SCRIPT HERE ########
-checkOperatingSystem
-determinePythonVersion
-
-# Get the absolute path where this script is located
-scriptFolder=$(cd $(dirname "${0}") && pwd)
-# Changes to the directory where the script is located
-cd ${scriptFolder}
-
-# Determine what OS is being used.
-# - "$@" command line arguments are passed to the Python program.
-if [[ "${operatingSystem}" = "cygwin" ]] || [[ "${operatingSystem}" = "linux" ]] || [[ "${operatingSystem}" = "macos" ]]; then
-	# Cygwin, Linux and macOS
-	# Use py command
-	if [[ ${usePy} = "true" ]]; then
-		py structure_call_analysis.py "$@"
-	# Use python3 command
-	else
-		python3 structure_call_analysis.py "$@"
-	fi
-elif [[ "${operatingSystem}" = "mingw" ]]; then
-	# Git Bash
-	# Use py command
-	if [[ ${usePy} = "true" ]]; then
-		py structure_call_analysis.py "$@"
-	# Use the python executable
-	else
-		# We need to know the absolute path of where python.exe is located
-		pythonPath=$(which python)
-		# Finally, put quotes around the file path in case a directory has a space in
-		# its name
-		"${pythonPath}" structure_call_analysis.py "$@"
-	fi
-fi
---
+++ scraping_scripts/submit_requests_parallel.py
@@ -0,0 +1,60 @@
+from mpi4py import MPI
+import math
+import os
+import pandas as pd
+
+rights = pd.read_csv('../data/CDSS_WaterRights.csv', dtype='str')
+try:
+    f = open('next_right', 'r')
+    next_right = f.read()
+    first_index = int(next_right.split()[0])
+except:
+    first_index = 0
+rights_to_request=[]
+wdids = []
+dates = []
+total_lines = 0
+for index, row in rights.iloc[first_index:].iterrows():
+    total_lines+=int(row['Lines'])
+    if total_lines<200:
+        rights_to_request.append(row['Priority Admin No'])
+        wdids.append(row['WDID'])
+        dates.append(row['Adjudication Date'])
+    else:
+        with open('next_right','w') as f:
+            f.write(str(index)+' '+row['Priority Admin No'])
+        break
+rights_no=len(rights_to_request)
+
+# =============================================================================
+# Start parallelization
+# =============================================================================
+
+# Begin parallel simulation
+comm = MPI.COMM_WORLD
+
+# Get the number of processors and the rank of processors
+rank = comm.rank
+nprocs = comm.size
+
+# Determine the chunk which each processor will need to do
+count = int(math.floor(rights_no / nprocs))
+remainder = rights_no % nprocs
+
+# Use the processor rank to determine the chunk of work each processor will do
+if rank < remainder:
+    start = rank * (count + 1)
+    stop = start + count + 1
+else:
+    start = remainder * (count + 1) + (rank - remainder) * count
+    stop = start + count
+
+# =============================================================================
+# Go though all rights for each processor
+# =============================================================================
+for k in range(start, stop):
+    adminNo = rights_to_request[k]
+    WDID = wdids[k]
+    startdate = dates[k]
+    os.system("python3 structure_call_analysis.py --adminNo {} --WDID {} --format csv --endDate 09/30/2019 "
+              "--startDate {} --apiKey dBTnllEokTHF4+NOiEopD0e3MDFLP7vH --output {}.csv".format(adminNo,WDID,startdate,adminNo))
