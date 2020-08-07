180569c4ad1977dcb3bff32f6b8f2417a657a431
antonia-had <ah986@cornell.edu>
2020-08-07 17:45:27 -0400

    increased lines

--- scraping_scripts/structure_call_analysis.sh
+++ scraping_scripts/structure_call_analysis.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=5
 #SBATCH --export=ALL
-#SBATCH -t 0:10:00            # set max wallclock time
+#SBATCH -t 1:00:00            # set max wallclock time
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/shortage_network/bin/activate
