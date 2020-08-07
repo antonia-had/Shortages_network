cdaba50ab41418982b8eafee047bbcd9f7360e4c
antonia-had <ah986@cornell.edu>
2020-08-07 17:31:16 -0400

    parallelization debug

--- scraping_scripts/structure_call_analysis.sh
+++ scraping_scripts/structure_call_analysis.sh
@@ -2,12 +2,11 @@
 #SBATCH --job-name="CDSS_requests"
 #SBATCH --output="requests.out"
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --ntasks-per-node=5
 #SBATCH --export=ALL
 #SBATCH -t 0:10:00            # set max wallclock time
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/shortage_network/bin/activate
-mpirun -n 1 python3 submit_requests_parallel.py
+mpirun -n 5 python3 submit_requests_parallel.py
 
--- scraping_scripts/submit_requests_parallel.py
+++ scraping_scripts/submit_requests_parallel.py
@@ -1,64 +1,67 @@
 from mpi4py import MPI
-import math
 import os
 import pandas as pd
 
-rights = pd.read_csv('../data/CDSS_WaterRights.csv', dtype='str')
-try:
-    f = open('next_right', 'r')
-    next_right = f.read()
-    first_index = int(next_right.split()[0])
-except:
-    first_index = 0
-rights_to_request=[]
-wdids = []
-dates = []
-total_lines = 0
-for index, row in rights.iloc[first_index:].iterrows():
-    total_lines+=int(row['Lines'])
-    if total_lines<2000:
-        rights_to_request.append(row['Priority Admin No'])
-        wdids.append(row['WDID'])
-        dates.append(row['Adjudication Date'])
-    else:
-        with open('next_right','w') as f:
-            f.write(str(index)+' '+row['Priority Admin No'])
-        break
-
-rights_no=len(rights_to_request)
-print(rights_to_request)
-
-# =============================================================================
-# Start parallelization
-# =============================================================================
-
-# Begin parallel simulation
 comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-# Get the number of processors and the rank of processors
-rank = comm.rank
-nprocs = comm.size
-print(nprocs)
+'''Master to determine number of tasks for the day and divide among cores'''
+if rank == 0:
+    '''Identify rights to request so limit is not exceeded'''
+    rights = pd.read_csv('../data/CDSS_WaterRights.csv', dtype='str')
+    try:
+        f = open('next_right', 'r')
+        next_right = f.read()
+        first_index = int(next_right.split()[0])
+    except:
+        first_index = 0
+    rights_to_request = []
+    wdids = []
+    dates = []
+    total_lines = 0
+    for index, row in rights.iloc[first_index:].iterrows():
+        total_lines += int(row['Lines'])
+        if total_lines < 2000:
+            rights_to_request.append(row['Priority Admin No'])
+            wdids.append(row['WDID'])
+            dates.append(row['Adjudication Date'])
+        else:
+            # Print placeholder right to pick up from next time
+            with open('next_right', 'w') as f:
+                f.write(str(index) + ' ' + row['Priority Admin No'])
+            break
+    '''Divide tasks to be executed by each core'''
+    # determine the size of each sub-task
+    ave, res = divmod(len(rights_to_request), size)
+    counts = [ave + 1 if p < res else ave for p in range(size)]
 
-# Determine the chunk which each processor will need to do
-count = int(math.floor(rights_no / nprocs))
-remainder = rights_no % nprocs
+    # determine the starting and ending indices of each sub-task
+    starts = [sum(counts[:p]) for p in range(size)]
+    ends = [sum(counts[:p+1]) for p in range(size)]
 
-# Use the processor rank to determine the chunk of work each processor will do
-if rank < remainder:
-    start = rank * (count + 1)
-    stop = start + count + 1
+    # converts data into a list of arrays
+    rights_to_request = [rights_to_request[starts[p]:ends[p]] for p in range(size)]
+    wdids = [wdids[starts[p]:ends[p]] for p in range(size)]
+    dates = [dates[starts[p]:ends[p]] for p in range(size)]
+
 else:
-    start = remainder * (count + 1) + (rank - remainder) * count
-    stop = start + count
+    rights_to_request = None
+    wdids = None
+    dates = None
 
-# =============================================================================
-# Go though all rights for each processor
-# =============================================================================
-for k in range(start, stop):
+'''Scatter tasks from master to every core'''
+rights_to_request = comm.scatter(rights_to_request, root=0)
+wdids = comm.scatter(wdids, root=0)
+dates = comm.scatter(dates, root=0)
+
+'''Every core goes through its assigned tasks'''
+print('Process {} has to retrieve rights:'.format(rank), rights_to_request)
+for k in range(len(rights_to_request)):
     adminNo = rights_to_request[k]
-    print(adminNo)
     WDID = wdids[k]
     startdate = dates[k]
     os.system("python3 url_request.py --adminNo {} --WDID {} --format csv --endDate 09/30/2019 "
-              "--startDate {} --apiKey dBTnllEokTHF4+NOiEopD0e3MDFLP7vH --output {}.csv".format(adminNo,WDID,startdate,adminNo))
+              "--startDate {} --apiKey dBTnllEokTHF4+NOiEopD0e3MDFLP7vH --output {}.csv".format(adminNo, WDID,
+                                                                                                startdate, adminNo))
+
