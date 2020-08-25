16777310b3606adc1cfb9a92646227b38da83086
antonia-had <ah986@cornell.edu>
2020-08-25 16:46:28 -0400

    test

--- scraping_scripts/structure_call_analysis.sh
+++ scraping_scripts/structure_call_analysis.sh
@@ -6,8 +6,6 @@
 #SBATCH --export=ALL
 #SBATCH -t 1:00:00            # set max wallclock time
 
-source ~/.bashrc
-source /etc/profile
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/shortage_network/bin/activate
 mpirun -n 5 python3 submit_requests_parallel.py
